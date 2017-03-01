1373e39e611d26d48d8f820da58e9c0ca2894454
Lan <494651913@qq.com>
2017-03-01 20:59:46 +0800

    精简路由 通过urls传参精简路由

---
+++ data_process/monitor_view.py
@@ -0,0 +1,302 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import rrd_helper
+import time
+import datetime
+from django.core.cache import cache
+from django.core.exceptions import ObjectDoesNotExist
+from models import Host, TrustHost, DeviceInfo, HostThreshold, \
+    ProcessInfo, IpPacket, FileInfo, MediaInfo, WarningHistory, IpPacketsRules, \
+    FileRules
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib import auth
+import demjson
+from django.conf import settings
+
+
+def get_mediainfo(request):
+    host = request.GET.get('h')
+    time_range = request.GET.get('r', '')
+
+    time_dict = settings.TIME_RANGE
+    if time_range in time_dict:
+        start = time_dict[time_range]
+    else:
+        start = time_dict['hour']
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        medias = MediaInfo.objects.filter(host__exact=host_info,
+                                          time__gt=datetime.datetime.fromtimestamp(time.time() - start[0]))
+    except ObjectDoesNotExist:
+        pass
+
+    medias_info = []
+    for e in medias:
+        one_media_info = {}
+        one_media_info['time'] = e.time.strftime("%Y-%m-%d %H:%M:%S")
+        one_media_info['media_name'] = e.media_name
+        one_media_info['media_size'] = e.media_size
+        one_media_info['io_type'] = e.io_type
+        one_media_info['operate_file'] = e.operate_file
+        medias_info.append(one_media_info)
+
+    media_json = demjson.encode(medias_info)
+    return HttpResponse(content=media_json)
+
+
+def get_fileinfo(request):
+    host = request.GET.get('h')
+    time_range = request.GET.get('r', '')
+
+    time_dict = settings.TIME_RANGE
+    if time_range in time_dict:
+        start = time_dict[time_range]
+    else:
+        start = time_dict['hour']
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        files = FileInfo.objects.filter(host__exact=host_info,
+                                          time__gt=datetime.datetime.fromtimestamp(time.time() - start[0]))
+    except ObjectDoesNotExist:
+        pass
+
+    files_info = []
+    for e in files:
+        one_file_info = {}
+        one_file_info['time'] = e.time.strftime("%Y-%m-%d %H:%M:%S")
+        one_file_info['file_name'] = e.file_name
+        one_file_info['user'] = e.user
+        one_file_info['operate_type'] = e.operate_type
+        one_file_info['modify_size'] = e.modify_size
+        files_info.append(one_file_info)
+
+    file_json = demjson.encode(files_info)
+    return HttpResponse(content=file_json)
+
+
+def get_processinfo(request):
+    host = request.GET.get('h')
+    time_range = request.GET.get('r', '')
+
+    time_dict = settings.TIME_RANGE
+    if time_range in time_dict:
+        start = time_dict[time_range]
+    else:
+        start = time_dict['hour']
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        process = ProcessInfo.objects.filter(host__exact=host_info,
+                                          time__gt=datetime.datetime.fromtimestamp(time.time() - start[0]))
+    except ObjectDoesNotExist:
+        pass
+
+    process_info = []
+    for e in process:
+        one_process_info = {}
+        one_process_info['time'] = e.time.strftime("%Y-%m-%d %H:%M:%S")
+        one_process_info['process_name'] = e.process_name
+        one_process_info['process_id'] = e.process_id
+        one_process_info['user'] = e.user
+        one_process_info['boottime'] = e.boottime.strftime("%Y-%m-%d %H:%M:%S")
+        one_process_info['runtime'] = e.runtime
+        one_process_info['used_ports'] = e.used_ports
+        process_info.append(one_process_info)
+
+    process_json = demjson.encode(process_info)
+    return HttpResponse(content=process_json)
+
+
+def get_warnings(request):
+    host = request.GET.get('h')
+    time_range = request.GET.get('r', '')
+
+    time_dict = settings.TIME_RANGE
+    if time_range in time_dict:
+        start = time_dict[time_range]
+    else:
+        start = time_dict['hour']
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        warnings = WarningHistory.objects.filter(host__exact=host_info,
+                                          time__gt=datetime.datetime.fromtimestamp(time.time() - start[0]))
+    except ObjectDoesNotExist:
+        pass
+
+    warnings_info = []
+    for e in warnings:
+        one_warning_info = {}
+        one_warning_info['time'] = e.time.strftime("%Y-%m-%d %H:%M:%S")
+        one_warning_info['warning_type'] = e.warning_type
+        one_warning_info['warning_content'] = e.warning_content
+        one_warning_info['warning_level'] = e.warning_level
+        warnings_info.append(one_warning_info)
+
+    warning_json = demjson.encode(warnings_info)
+    return HttpResponse(content=warning_json)
+
+
+def get_ippackets(request):
+    host = request.GET.get('h')
+    time_range = request.GET.get('r', '')
+
+    time_dict = settings.TIME_RANGE
+    if time_range in time_dict:
+        start = time_dict[time_range]
+    else:
+        start = time_dict['hour']
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        ip_packets = IpPacket.objects.filter(host__exact=host_info,
+                                             time__gt=datetime.datetime.fromtimestamp(time.time() - start[0]))
+    except ObjectDoesNotExist:
+        pass
+
+    ip_packets_info = []
+    for e in ip_packets:
+        one_ip_packet = {}
+        one_ip_packet['time'] = e.time.strftime("%Y-%m-%d %H:%M:%S")
+        one_ip_packet['app_name'] = e.app_name
+        one_ip_packet['send_port'] = e.send_port
+        one_ip_packet['recv_ip'] = e.recv_ip
+        one_ip_packet['recv_port'] = e.recv_port
+        ip_packets_info.append(one_ip_packet)
+
+    ip_packets_json = demjson.encode(ip_packets_info)
+    return HttpResponse(content=ip_packets_json)
+
+
+def get_ippackets_rules(request):
+    host = request.GET.get('h')
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        ip_packets = IpPacketsRules.objects.filter(host__exact=host_info)
+    except ObjectDoesNotExist:
+        pass
+
+    ip_packets_info = []
+    for e in ip_packets:
+        one_ip_packet = {}
+        one_ip_packet['id'] = e.id
+        one_ip_packet['rule'] = e.rule_chain
+        one_ip_packet['ip'] = e.ip
+        ip_packets_info.append(one_ip_packet)
+
+    ip_packets_json = demjson.encode(ip_packets_info)
+    return HttpResponse(content=ip_packets_json)
+
+
+@csrf_exempt
+def add_ippackets_rules(request):
+    host = request.GET.get('h')
+    if request.method == "POST" and request.is_ajax:
+        chain = request.POST.get('chain')
+        ip = request.POST.get('ip')
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        ip_packets = IpPacketsRules.objects.create(host=host_info,
+                                                   rule_chain=chain, ip=ip)
+        ip_packets.save()
+
+        return HttpResponse('saved')
+    except ObjectDoesNotExist:
+        pass
+
+    return HttpResponse('failed')
+
+
+@csrf_exempt
+def remove_ippackets_rules(request):
+    host = request.GET.get('h')
+    datas = demjson.decode(request.body)
+    print datas, host
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+    except ObjectDoesNotExist:
+        return HttpResponse('failed')
+
+    for data in datas:
+        print data
+        try:
+            ip_packets = IpPacketsRules.objects.filter(host__exact=host_info,
+                                                       ip=data['ip'],
+                                                       rule_chain=data['rule'])
+            ip_packets.delete()
+            pass
+        except ObjectDoesNotExist:
+            pass
+
+    return HttpResponse('delete')
+
+
+def get_file_rules(request):
+    host = request.GET.get('h')
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        files = FileRules.objects.filter(host__exact=host_info)
+    except ObjectDoesNotExist:
+        pass
+
+    files_info = []
+    for e in files:
+        one_file = {}
+        one_file['id'] = e.id
+        one_file['file'] = e.file
+        one_file['permission'] = e.permission
+        files_info.append(one_file)
+
+    files_json = demjson.encode(files_info)
+    return HttpResponse(content=files_json)
+
+
+@csrf_exempt
+def add_file_rules(request):
+    host = request.GET.get('h')
+    if request.method == "POST" and request.is_ajax:
+        file = request.POST.get('file')
+        permission = request.POST.get('permission')
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+        file_rule = FileRules.objects.create(host=host_info,
+                                              file=file,
+                                              permission=permission)
+        file_rule.save()
+
+        return HttpResponse('saved')
+    except ObjectDoesNotExist:
+        pass
+
+    return HttpResponse('failed')
+
+
+@csrf_exempt
+def remove_file_rules(request):
+    host = request.GET.get('h')
+    datas = demjson.decode(request.body)
+    print datas, host
+
+    try:
+        host_info = Host.objects.get(hostname=host)
+    except ObjectDoesNotExist:
+        return HttpResponse('failed')
+
+    for data in datas:
+        print data
+        try:
+            file_rules = FileRules.objects.filter(host__exact=host_info,
+                                                       file=data['file'],
+                                                       permission=data['permission'])
+            file_rules.delete()
+        except ObjectDoesNotExist:
+            pass
+
+    return HttpResponse('delete')
--- monitor/urls.py
+++ monitor/urls.py
@@ -14,27 +14,41 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from data_process import views as server_view
+from rest_framework import routers, serializers, viewsets
+from data_process.models import MyUser
 
 admin.site.site_header = u'网络监控平台'
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = MyUser
+        fields = ('url', 'username', 'email', 'is_staff')
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = MyUser.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
 urlpatterns = [
     url(r'^$', server_view.home),
+    url(r'^ss/', server_view.test),
     url(r'^hostgraph/$', server_view.host_graphs),
-    url(r'^hostgraph/get_warnings$', server_view.get_warnings),
-    url(r'^hostgraph/get_ippackets$', server_view.get_ippackets),
-    url(r'^hostgraph/get_mediainfo$', server_view.get_mediainfo),
-    url(r'^hostgraph/get_fileinfo$', server_view.get_fileinfo),
-    url(r'^hostgraph/get_processinfo$', server_view.get_processinfo),
+    url(r'^hostgraph/(?P<monitor_info>\w+)$', server_view.get_monitor_data),
     url(r'^safe_strategy/$', server_view.safe_strategy),
-    url(r'^safe_strategy/get_ippackets_rules$', server_view.get_ippackets_rules),
-    url(r'^safe_strategy/add_ippackets_rules', server_view.add_ippackets_rules),
-    url(r'^safe_strategy/remove_ippackets_rules', server_view.remove_ippackets_rules),
-    url(r'^safe_strategy/get_file_rules$', server_view.get_file_rules),
-    url(r'^safe_strategy/add_file_rules', server_view.add_file_rules),
-    url(r'^safe_strategy/remove_file_rules', server_view.remove_file_rules),
+    url(r'^safe_strategy/(?P<strategy_method>\w+)$', server_view.strategy_method),
     url(r'^(?:hostgraph/)?image/$', server_view.image),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', server_view.login),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
